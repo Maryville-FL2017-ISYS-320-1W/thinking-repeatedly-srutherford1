cfca107dd71b61456a717b951e3443899e68c591
Samantha Rutherford <33076449+srutherford1@users.noreply.github.com>
2017-12-11 16:06:46 -0500

    OddStuff

--- src/P5_OddStuff.java
+++ src/P5_OddStuff.java
@@ -1,21 +1,26 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
-*/
+  	Name(s): Samantha Rutherford
+  	Date: 12/10/2017
+*//
 
-// 1. Your predicted output 
+// 1. The output will print out numbers until the number is equal to 4/
+// it will print out 4 and 2.
 
 
 
 public class P5_OddStuff {
 
-	public static void main(String[] args) {
-
-	}
-
+	public static void main( String[] args ) {
+        int number = 4;
+        for( int count = 1; count <= number; count++ ) {
+            System.out.println( number );
+            number = number / 2;
+        }
+    }
 }
 
 
 
-// 3. Were you correct? Explain any differences and what you learned.
+// 3. I was correct that it would print out those numbers but thought it would print them out more than
+// once.
