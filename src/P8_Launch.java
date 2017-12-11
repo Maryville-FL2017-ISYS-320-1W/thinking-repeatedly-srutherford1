8fafbd35013fe9d7fa846d1fcf6353e6421a4e22
Samantha Rutherford <33076449+srutherford1@users.noreply.github.com>
2017-12-11 16:10:03 -0500

    Launch

--- src/P8_Launch.java
+++ src/P8_Launch.java
@@ -1,21 +1,25 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Samantha Rutherford
+  	Date: 12/10/2017
 */
 
-// 1. Your predicted output 
+// 1. This will do a count down of 5, 4, 3, 2, 1, Blastoff! 
 
 
 
 public class P8_Launch {
 
-	public static void main(String[] args) {
-
-	}
+	public static void main( String[] args ) {
+        System.out.println("T-minus ");
+        for( int i = 5; i >= 1; i-- ) {
+            System.out.print( i + ", " );
+        }
+        System.out.println("Blastoff!");
+    }
 
 }
 
 
 
-// 3. Were you correct? Explain any differences and what you learned.
+// 3. I am correct about the countdown. Forgot about the T-minus at the beginning.
