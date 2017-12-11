bdfcc693fa48a19df415610c3e046d652dcccce9
Samantha Rutherford <33076449+srutherford1@users.noreply.github.com>
2017-12-11 16:19:54 -0500

    NumbersLeft

--- src/P12_NumbersLeft.java
+++ src/P12_NumbersLeft.java
@@ -7,7 +7,25 @@
 public class P12_NumbersLeft {
 
 	public static void main(String[] args) {
-
+		for (int i=0; i<6; i++) {
+			for (int j=0; j<=i; j++) {
+				System.out.print(i);
+			}
+			System.out.println(" ");
+		}
 	}
+/*
+ * Output
+ * 
+ * 0 
+ * 11 
+ * 222 
+ * 3333 
+ * 44444 
+ * 555555
+ * 
+ * Think I'm getting this because I made int j to equal 0 and not one but still cool. 
+ * Just longer.
+ */
 
 }
